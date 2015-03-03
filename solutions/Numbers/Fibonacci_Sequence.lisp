2acebc1a8e73fd067f4743562080308963ec4597
Michael Kenney <michael.kenney@returnpath.net>
2015-03-03 00:00:51 -0700

    Fibonacci Sequence

--- solutions/Numbers/Fibonacci_Sequence.lisp
+++ solutions/Numbers/Fibonacci_Sequence.lisp
@@ -7,14 +7,14 @@
 ;;;
 
 (let ((iterations nil) (cur 1) (last 1) (tmp nil))
-    (if (= 0 (list-length *args*))
-        (progn
-            (format t "How many Fibonacci numbers do you want? ")
-            (setq iterations (parse-integer (read-line t))))
-        (setq iterations (parse-integer (nth 0 *args*))))
+	(if (= 0 (list-length *args*))
+		(progn
+			(format t "How many Fibonacci numbers do you want? ")
+			(setq iterations (parse-integer (read-line t))))
+		(setq iterations (parse-integer (nth 0 *args*))))
 
-    (loop for a from 1 to iterations do
-        (format t "~A~%" cur)
-        (setq tmp cur)
-        (setq cur (+ cur last))
-        (setq last tmp)))
+	(loop for a from 1 to iterations do
+		(format t "~A~%" cur)
+		(setq tmp cur)
+		(setq cur (+ cur last))
+		(setq last tmp)))
